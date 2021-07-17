b26466fc30eeef3a2636fd115b21821021018a4c
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2021-07-17 17:22:59 +0900

    Add files via upload

---
+++ Nutrition.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+import pandas as pd
+
+df = pd.read_csv("FoodList.csv", encoding = 'utf-8')
+d_f=pd.DataFrame(df)
+d_f
+
+
+# In[37]:
+
+
+df_drop = d_f.dropna(axis = 1)
+
+
+# In[51]:
+
+
+df_drop.to_csv("FoodList.csv",encoding='utf-8')
+
